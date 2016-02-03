381e4e2fc22d1b769134cc110d829403b36c6ec6
chimoso <ianboersma@ians-macbook-pro.local>
2016-02-02 19:12:37 -0700

    major updates, including sound effect and background music and the ability for the players to enter their names and choose their characters

---
+++ NerdClash/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  NerdClash
+//
+//  Created by Ian Boersma on 1/31/16.
+//  Copyright © 2016 iboersma. All rights reserved.
+//
+
+import Foundation
--- NerdClash/Player.swift
+++ NerdClash/Player.swift
@@ -1,5 +1,5 @@
 //
-//  Player.swift
+//  Character.swift
 //  NerdClash
 //
 //  Created by Ian Boersma on 1/22/16.
@@ -8,9 +8,33 @@
 
 import Foundation
 
-class Player: Character {
+class Character {
     
+    private var _hp: Int = 100
+    private var _attackPwr: Int
     private var _name = "Player"
+    
+    //getters
+    var hp: Int {
+        get {
+            return _hp
+        }
+    }
+    
+//    var attackPwr: Int {
+//        get {
+//            // Generate random attack HP between 0 and 20
+//            let rand = Int(arc4random_uniform(20))
+//            self._attackPwr = rand
+//            return rand
+//        }
+//    }
+    
+    var attackPwr: Int {
+        get {
+            return _attackPwr
+        }
+    }
     
     var name: String {
         get {
@@ -18,10 +42,39 @@ class Player: Character {
         }
     }
     
-    convenience init(name: String, hp: Int, attackPwr: Int) {
-        
-        self.init(startingHp: hp, attackPwr: attackPwr)
-        
-        _name = name
+    var isAlive: Bool {
+        get {
+            if _hp <= 0 {
+                return false
+            } else {
+                return true
+            }
+        }
     }
+    
+    init(name: String, startingHp: Int, attackPwr: Int) {
+        self._hp = startingHp
+        self._attackPwr = attackPwr
+
+    }
+    
+//    func receiveAttack(attackPwr: Int) {
+//        self._hp -= attackPwr
+// 
+//    }
+    
+    func performAttack(attackingPlayer: Character, playerBeingAttacked: Character) {
+        // Randomize attack power
+        attackingPlayer.randomizeAttackPwr()
+        
+        if playerBeingAttacked._hp <= 0 {
+            playerBeingAttacked._hp -= attackingPlayer._attackPwr
+        }
+    }
+    
+    // Randomize the attack power of player
+    func randomizeAttackPwr() {
+        self._attackPwr = 20
+    }
+    
 }
